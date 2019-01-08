1d1298ff29a7d5dfba9a39baf75fb0228e4d04f9
qed0711 <qdizon@gmail.com>
2019-01-08 10:01:44 -0500

    apollo steup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ApolloClient } from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory'; // included with apollo boot
+import { HttpLink } from 'apollo-link-http'; // included with apollo boot
+import {ApolloProvider} from 'react-apollo';
+
+// Apollo Client Setup
+const client = new ApolloClient({
+    link: new HttpLink({uri: "http://localhost:4000/graphql"}),
+    cache: new InMemoryCache()
+  }
+)
+
+
+// COMPONENTS
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client} >
+        <div className="App">
+          <p>Music Fundamentals</p>
+          <p>A learning tool for basic and intermediate music concepts</p>
+        </div>
+      </ApolloProvider>
     );
   }
 }
